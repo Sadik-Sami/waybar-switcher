f577bd6f59944bbc04d203741ede711642bfd1f6
Sadik-Sami <miasimanto@gmail.com>
2025-12-27 14:10:11 +0600

    rebase with dynamic theme

--- extras/waybar/styles/palccod-v2/scripts/disk-smssd.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Get disk usage for /smssd partition
-DISK_USAGE=$(df -h /smssd | awk 'NR==2 {print $4}')
-
-# Output the free space with folder icon (nerd font)
-echo -e "\uf07c ${DISK_USAGE}"
